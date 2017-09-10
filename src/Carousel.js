b725e9f9e02d601d8f7af68c014bdebe730ed725
bonzatina <bonez@mail.ru>
2017-09-10 14:27:10 +0200

    start with slick

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,16 +1,19 @@
 
 import React, {Component} from 'react';
-import { CSSTransitionGroup } from 'react-transition-group'
+import Slider from 'react-slick';
 
-import lion from './lion.jpeg';
 
 import './Carousel.css';
 
-const carouselImages = [
-    lion,
-    lion,
-    <div style={{width: '100px'}}>dfdf</div>
+const carouselItems = [
+    {id: 1, img: '', title: 'Product Title 1', prise: '14,95 $', description: 'Lorem ipsum sit dolor lorem amet ipsum sit dolor lorem'},
+    {id: 2, img: '', title: 'Product Title 2', prise: '14,95 $', description: 'Lorem ipsum sit dolor lorem amet ipsum sit dolor lorem'},
+    {id: 3, img: '', title: 'Product Title 3', prise: '14,95 $', description: 'Lorem ipsum sit dolor lorem amet ipsum sit dolor lorem'},
+    {id: 4, img: '', title: 'Product Title 4', prise: '14,95 $', description: 'Lorem ipsum sit dolor lorem amet ipsum sit dolor lorem'},
+    {id: 5, img: '', title: 'Product Title 5', prise: '14,95 $', description: 'Lorem ipsum sit dolor lorem amet ipsum sit dolor lorem'},
+    {id: 6, img: '', title: 'Product Title 6', prise: '14,95 $', description: 'Lorem ipsum sit dolor lorem amet ipsum sit dolor lorem'},
 ];
+
 
 
 
@@ -19,194 +22,26 @@ export default class CarouselComponent extends Component {
 
 
     render() {
+        const settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1
+        };
         return (
             <div className="main">
-                <Carousel images={carouselImages} />
+                <Slider {...settings}>
+                    <div><h3>1</h3></div>
+                    <div><h3>2</h3></div>
+                    <div><h3>3</h3></div>
+                    <div><h3>4</h3></div>
+                    <div><h3>5</h3></div>
+                    <div><h3>6</h3></div>
+                </Slider>
             </div>
         );
     }
 }
 
-
-
-
-
-
-var Carousel = React.createClass({
-
-    propTypes: {
-        images: React.PropTypes.arrayOf([
-            React.PropTypes.string
-        ]).isRequired,
-        showThumbnails: React.PropTypes.bool,
-        slideshowActive: React.PropTypes.bool,
-        slideshowDelay: React.PropTypes.number
-    },
-
-    getDefaultProps: function () {
-        return {
-            defaultSelectedIndex: 0,
-            showThumbnails: true,
-            slideshowActive: false,
-            slideshowDelay: 4000
-        };
-    },
-
-    getInitialState: function () {
-        return {
-            animationDirection: 'previous',
-            selectedIndex: this.props.defaultSelectedIndex
-        };
-    },
-
-    componentDidMount: function() {
-        if (this.props.slideshowActive) {
-            this.progressSlideshow();
-        }
-    },
-
-    render: function () {
-        var Animation = CSSTransitionGroup;
-
-        return (
-            <div {...this.getProps()}>
-                <div className="carousel--image">
-                    {this.renderArrow('previous')}
-                    <Animation transitionName={'animation--' + this.state.animationDirection}>
-                        {this.renderCurrentImage()}
-                        {this.renderCurrentImage()}
-                    </Animation>
-                    {this.renderArrow('next')}
-                </div>
-
-            </div>
-        );
-    },
-
-    renderCurrentImage: function () {
-        var selected = this.state.selectedIndex;
-        var props = {
-            key: selected,
-            src: this.props.images[selected]
-        };
-
-        return (<div>
-                <img style={{width: '100px'}} {...props} />
-                <p>{this.state.selectedIndex}</p>
-            </div>
-        );
-    },
-
-    renderArrow: function (direction) {
-        var props = {
-            className: 'carousel--arrow-' + direction,
-            onClick: this.goInDirection.bind(null, direction)
-        };
-
-        return (
-            <div {...props} />
-        );
-    },
-
-    renderThumbs: function () {
-        var thumbnails = null;
-
-        if (this.props.showThumbnails) {
-            thumbnails = (
-                <div className="carousel--thumbs">
-                    {this.props.images.map(this.renderThumb)}
-                </div>
-            );
-        }
-
-        return thumbnails;
-    },
-
-    renderThumb: function (src, index) {
-        var selected = (index === this.state.selectedIndex) ? ' carousel--selected' : '';
-        var props = {
-            className: 'carousel--thumb' + selected,
-            key: index,
-            onClick: this.handleThumbClick.bind(null, index),
-            src: src
-        }
-        return <img {...props} />;
-    },
-
-    getProps: function () {
-        var props = {
-            className:'carousel',
-            onKeyDown: this.handleKeyDown,
-            tabIndex:'0'
-        };
-
-        if (this.props.slideshowActive) {
-            props.onMouseEnter = this.handleMouseEnter;
-            props.onMouseLeave = this.handleMouseLeave;
-        }
-
-        return props;
-    },
-
-    handleKeyDown: function (event) {
-        var left = 37;
-        var up = 38;
-        var right = 39;
-        var down = 40;
-        var key = event.which;
-
-        if (key === down || key === left) {
-            this.goInDirection('previous');
-        } else if (key === up || key === right) {
-            this.goInDirection('next');
-        }
-    },
-
-    handleMouseEnter: function () {
-        clearTimeout(this.timeout);
-    },
-
-    handleMouseLeave: function () {
-        this.progressSlideshow();
-    },
-
-    handleThumbClick: function (index) {
-        this.goToIndex(index);
-    },
-
-    progressSlideshow: function () {
-        this.setState({animationDirection: 'next'});
-
-        this.timeout = setTimeout(function () {
-            this.goInDirection('next');
-            this.progressSlideshow();
-        }.bind(this), this.props.slideshowDelay);
-    },
-
-    goToIndex: function (index) {
-        var direction = (this.state.selectedIndex > index ) ? 'previous' : 'next';
-
-        this.setState({
-            animationDirection: direction,
-            selectedIndex: index
-        });
-    },
-
-    goInDirection: function (direction) {
-        var totalImages = this.props.images.length;
-        var modifier = (direction === 'next') ? 1 : -1;
-        var newIndex = this.state.selectedIndex + modifier;
-
-        if (newIndex === totalImages) {
-            newIndex = 0;
-        } else if (newIndex === -1) {
-            newIndex = totalImages - 1;
-        }
-
-        this.setState({
-            animationDirection: direction,
-            selectedIndex: newIndex
-        });
-    }
-});
 
